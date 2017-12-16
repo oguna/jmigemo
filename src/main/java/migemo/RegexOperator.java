592c0d4d1edfb5beaad9d192ced017d34384b33a
oguna <nao.lk.118@gmail.com>
2017-12-16 13:36:41 +0900

    regexoperatorのフィールド変数名を正規表現に準じた名前に変更

--- src/main/java/migemo/RegexOperator.java
+++ src/main/java/migemo/RegexOperator.java
@@ -4,18 +4,18 @@ import java.util.Objects;
 
 public class RegexOperator {
     public final String or;
-    public final String nestIn;
-    public final String nestOut;
-    public final String selectIn;
-    public final String selectOut;
+    public final String beginGroup;
+    public final String endGroup;
+    public final String beginClass;
+    public final String endClass;
     public final String newline;
 
-    public RegexOperator(String or, String nestIn, String nestOut, String selectIn, String selectOut, String newline) {
+    public RegexOperator(String or, String beginGroup, String endGroup, String beginClass, String endClass, String newline) {
         this.or = Objects.requireNonNull(or);
-        this.nestIn = Objects.requireNonNull(nestIn);
-        this.nestOut = Objects.requireNonNull(nestOut);
-        this.selectIn = Objects.requireNonNull(selectIn);
-        this.selectOut = Objects.requireNonNull(selectOut);
+        this.beginGroup = Objects.requireNonNull(beginGroup);
+        this.endGroup = Objects.requireNonNull(endGroup);
+        this.beginClass = Objects.requireNonNull(beginClass);
+        this.endClass = Objects.requireNonNull(endClass);
         this.newline = newline;
     }
 
