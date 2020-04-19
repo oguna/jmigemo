ee2826e81df9557d899ba3bb270c2e835a86e792
oguna <nao.lk.118@gmail.com>
2020-04-19 22:19:32 +0900

    辞書生成を効率化

--- src/main/java/migemo/LOUDSTrieBuilder.java
+++ src/main/java/migemo/LOUDSTrieBuilder.java
@@ -1,94 +1,69 @@
 package migemo;
 
 import java.util.Arrays;
-import java.util.Objects;
+import java.util.BitSet;
 
 public class LOUDSTrieBuilder {
     public static LOUDSTrie build(String[] keys) {
-        return build(keys, null);
+        return build(keys, new int[keys.length]);
     }
 
-    public static LOUDSTrie build(String[] keys, int[] generatedIndex) {
-        Objects.requireNonNull(keys);
-        int[] memo;
-        if (generatedIndex == null) {
-            memo = new int[keys.length];
-        } else if (generatedIndex.length == keys.length) {
-            memo = generatedIndex;
-        } else {
-            throw new IllegalArgumentException();
+    public static LOUDSTrie build(String[] keys, int[] nodes) {
+        if (keys == null || nodes == null || nodes.length != keys.length) {
+            throw new RuntimeException();
         }
-        for (int i = 0; i < keys.length; i++) {
-            if (keys[i] == null) {
-                throw new IllegalArgumentException();
-            }
-            if (i > 0 && keys[i - 1].compareTo(keys[i]) >= 0) {
-                throw new IllegalArgumentException();
+        for (int i = 0; i < keys.length - 1; i++) {
+            if (keys[i].compareTo(keys[i + 1]) >= 0) {
+                throw new RuntimeException();
             }
         }
-        Arrays.fill(memo, 1);
-        int offset = 0;
+        Arrays.fill(nodes, 1);
+        int cursor = 0;
         int currentNode = 1;
         StringBuilder edges = new StringBuilder();
-        edges.append(0);
-        edges.append(0);
-        int[] childSizes = new int[128];
+        edges.append(' ');
+        edges.append(' ');
+        BitSet louds = new BitSet();
+        int loudsIndex = 0;
+        louds.set(loudsIndex++, true);
         while (true) {
             char lastChar = 0;
             int lastParent = 0;
             int restKeys = 0;
             for (int i = 0; i < keys.length; i++) {
-                if (memo[i] < 0) {
+                if (keys[i].length() < cursor) {
                     continue;
                 }
-                if (keys[i].length() <= offset) {
-                    memo[i] = -memo[i];
+                if (keys[i].length() == cursor) {
+                    louds.set(loudsIndex++, false);
+                    lastParent = nodes[i];
+                    lastChar = 0;
                     continue;
                 }
-                char currentChar = keys[i].charAt(offset);
-                int currentParent = memo[i];
-                if (lastChar != currentChar || lastParent != currentParent) {
-                    if (childSizes.length <= memo[i]) {
-                        childSizes = Arrays.copyOf(childSizes, childSizes.length * 2);
-                    }
-                    childSizes[memo[i]]++;
-                    currentNode++;
+                char currentChar = keys[i].charAt(cursor);
+                int currentParent = nodes[i];
+                if (lastParent != currentParent) {
+                    louds.set(loudsIndex++, false);
+                    louds.set(loudsIndex++, true);
                     edges.append(currentChar);
-                    lastChar = currentChar;
-                    lastParent = currentParent;
+                    currentNode = currentNode + 1;
+                } else if (lastChar != currentChar) {
+                    louds.set(loudsIndex++, true);
+                    edges.append(currentChar);
+                    currentNode = currentNode + 1;
                 }
-                memo[i] = currentNode;
+                nodes[i] = currentNode;
+                lastChar = currentChar;
+                lastParent = currentParent;
                 restKeys++;
             }
             if (restKeys == 0) {
                 break;
             }
-            offset++;
+            cursor++;
         }
-        //System.out.println(Arrays.toString(memo));
-        //System.out.println(edges.toString());
-        //System.out.println(childSizes.toString());
-        for (int i = 0; i < memo.length; i++) {
-            memo[i] = -memo[i];
-        }
-        int numOfChildren = 0;
-        for (int i = 1; i <= currentNode; i++) {
-            numOfChildren += childSizes[i];
-        }
-        int numOfNodes = currentNode;
-        long[] bitVectorWords = new long[(numOfChildren + numOfNodes + 63 + 1) / 64];
-        int bitVectorIndex = 1;
-        bitVectorWords[0] = 1;
-        for (int i = 1; i <= currentNode; i++) {
-            bitVectorIndex++;
-            int childSize = childSizes[i];
-            for (int j = 0; j < childSize; j++) {
-                bitVectorWords[bitVectorIndex >> 6] |= 1L << (bitVectorIndex & 63);
-                bitVectorIndex++;
-            }
-        }
-
-        BitVector bitVector = new BitVector(bitVectorWords, bitVectorIndex);
+        long[] words = Arrays.copyOf(louds.toLongArray(), (loudsIndex + 63) / 64);
+        BitVector bitVector = new BitVector(words, loudsIndex);
         return new LOUDSTrie(bitVector, edges.toString().toCharArray());
     }
 }
